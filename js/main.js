690a6680f768439356636c6325163649ee2e5903
tubi55 <tubi55@nate.com>
2022-01-05 23:19:51 +0900

    활성화 버튼 재 클릭 방지

--- js/main.js
+++ js/main.js
@@ -7,17 +7,25 @@ window.addEventListener("load",()=>{
 
     const btns = document.querySelectorAll("main ul li");
 
-    btns.forEach((btn, index)=>{
+    btns.forEach(btn=>{
         btn.addEventListener("click",e=>{
             e.preventDefault();
 
-            const btn_a = e.currentTarget.querySelector("a");
-            const a_href = btn_a.getAttribute("href");
-            console.log(a_href);
-
-            grid.arrange({filter: a_href});
+            const isOn = e.currentTarget.classList.contains("on");
+            if(isOn) return;
+            activation(e);            
         })
     })
+
+    function activation(event){
+        for(let btn of btns) btn.classList.remove("on");
+        event.currentTarget.classList.add("on");
+
+        const btn_a = event.currentTarget.querySelector("a");
+        const a_href = btn_a.getAttribute("href");     
+
+        grid.arrange({filter: a_href});
+    }
 })
 
 
