8284ef80780a4991fc26fb2bc0aaa4f3adbb995e
tubi55 <tubi55@nate.com>
2022-01-05 22:21:00 +0900

    isotope 기능 적용

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-
+window.addEventListener("load",()=>{
+    new Isotope("section",{
+        itemSelector: "article",
+        columnWidth: "article",
+        transitionDuration: "0.5s"
+    })
+})
 
 
