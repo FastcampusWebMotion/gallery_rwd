43a04674caaa0eb09e4e0888390b9a05682f65c0
tubi55 <tubi55@nate.com>
2022-01-05 22:33:29 +0900

    필터링 기능 적용

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
 window.addEventListener("load",()=>{
-    new Isotope("section",{
+    const grid = new Isotope("section",{
         itemSelector: "article",
         columnWidth: "article",
         transitionDuration: "0.5s"
+    })
+
+    const btns = document.querySelectorAll("main ul li");
+
+    btns.forEach((btn, index)=>{
+        btn.addEventListener("click",e=>{
+            e.preventDefault();
+
+            const btn_a = e.currentTarget.querySelector("a");
+            const a_href = btn_a.getAttribute("href");
+            console.log(a_href);
+
+            grid.arrange({filter: a_href});
+        })
     })
 })
 
